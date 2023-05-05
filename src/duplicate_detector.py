773fd267682c32f4b6df46b24b201987786bdd0e
Shayan Shakiba <shayshakiba@gmail.com>
2023-05-05 22:31:56 +0330

    Add duplicate_detector.py

---
+++ src/duplicate_detector.py
@@ -0,0 +1,22 @@
+"""Handling history and duplicate detection for URLs and content."""
+
+
+import url_history
+import content_history
+from html_parser import ParsedContent
+
+
+def is_duplicate_url(url: str) -> bool:
+    return url_history.contains(url)
+
+
+def save_url(url: str) -> None:
+    url_history.add(url)
+
+
+def is_duplicate_content(content: ParsedContent) -> bool:
+    return content_history.contains(content.body)
+
+
+def save_content(content: ParsedContent) -> None:
+    content_history.add(content.body)
