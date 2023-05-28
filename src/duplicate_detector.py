ffb6d9323519112ae438dd00c1d32244d728b233
Shayan Shakiba <shayshakiba@gmail.com>
2023-05-28 20:57:06 +0330

    Update duplicate_detector.py

--- src/duplicate_detector.py
+++ src/duplicate_detector.py
@@ -3,20 +3,44 @@
 
 import content_history
 import url_history
-from html_parser import ParsedContent
+from page import Page
 
 
-def is_duplicate_url(url: str) -> bool:
-    return url_history.contains(url)
+def have_duplicate_url(page: Page) -> bool:
+    """Check if the smae URL has been discovered before.
+
+    If the URL isn't a duplicate, it will be added to the URL history as a side effect.
+
+    Args:
+        page (Page): The given page.
+            It should contain an initialized URL.
+
+    Returns:
+        bool: True if the URL is a duplicate.
+    """
+    if not url_history.contains(page.url):
+        url_history.add(page.url)
+
+        return False
+
+    return True
 
 
-def save_url(url: str) -> None:
-    url_history.add(url)
+def have_duplicate_content(page: Page) -> bool:
+    """Check if the same content has been discovered before.
 
+    If the content isn't a duplicate, it will be added to the content history as a side effect.
 
-def is_duplicate_content(content: ParsedContent) -> bool:
-    return content_history.contains(content.body)
+    Args:
+        page (Page): The given page.
+            It should contain an initialized parsed content.
 
+    Returns:
+        bool: True if the content is a duplicate.
+    """
+    if not content_history.contains(page.parsed_content.body):
+        content_history.add(page.parsed_content.body)
 
-def save_content(content: ParsedContent) -> None:
-    content_history.add(content.body)
+        return False
+
+    return True
